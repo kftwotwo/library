355d958b49c2919067654de891b61724624cba96
Andrew Accuardi and Kevin Finley <kevin@>
2016-12-07 11:52:34 -0800

    add initialize spec with pass

--- lib/authors.rb
+++ lib/authors.rb
@@ -1,28 +1,8 @@
 class Author
-  attr_reader(:name, :id)
+ attr_reader(:name, :id)
 
-  def initialize(attrs)
-    @name = attrs[:name]
-    @id = attrs[:id]
-  end
-
-  # class << self
-  #   def all
-  #     returned_authors = DB.exec('select * from authors')
-  #     authors = []
-  #     returned_authors.each() do |author|
-  #       name = author.fetch("name")
-  #       authors.push(Author.new({:name => name}))
-  #     end
-  #     authors
-  #   end
-  # end
-  #
-  # def save
-  #   DB.exec("INSERT INTO authors (id_authors, name) VALUES (#{@id or 'NULL'}, '#{@name}')")
-  # end
-  #
-  # def ==(other)
-  #   @name = other.name and @id = other.id
-  # end
+ def initialize(attrs)
+   @name = attrs[:name]
+   @id = attrs[:id]
+ end
 end
--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -1,41 +1,13 @@
 require('spec_helper')
 
-
-
 describe(Author) do
-  before :each  do
-    @andrew = Author.new({:name => "andrew"})
-    @kevin = Author.new({:name => "kevin"})
-    @author = Author.new({:name => "andrew"})
+  before :each do
+    @author = Author.new({:name => 'andrew'})
   end
   describe('#initialize') do
-    it "will return name" do
-      expect(@andrew.name).to eq "andrew"
-      expect(@andrew.id).to eq nil
+    it "will initialize name and id" do
+      expect(@author.name).to eq 'andrew'
+      expect(@author.id).to eq nil
     end
   end
-
-#   describe('#save') do
-#     it "will save into the database" do
-#       @andrew.save()
-#       expect(Author.all).to eq [@andrew]
-#     end
-#   end
-#
-#   describe(".all") do
-#     it "will return empty" do
-#       expect(Author.all()).to eq([])
-#     end
-#   end
-#
-#   describe('#==') do
-#     it "will compare if it is eqaul by name" do
-#       @andrew.save()
-#       @author.save()
-#       expect(@andrew).to eq @author
-#     end
-#   end
-
 end
-# TODO: save
-# TODO: all
